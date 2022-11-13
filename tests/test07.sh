bb9bc7008aa1e07c3647cf37ada1a96c77790faf
Les quatre cents coups <ssqr37@gmail.com>
2022-11-13 16:29:28 +1100

    Add all files

---
+++ tests/test07.sh
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+
+    <title>
+        COMP(2041|9044) 22T2 —
+        ass1_tigger/fri15-strings/5289988 ! test07.sh
+    </title><link rel="canonical" href="https://cgi.cse.unsw.edu.au/~cs2041/22T2/"/>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css"
+          integrity="sha256-aAr2Zpq8MZ+YA/D6JtRD3xtrwpEz2IqOS+pWD/7XKIw=" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="/~cs2041/22T2/flask.cgi/static/course.css?208401658889171.0="/>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"
+            integrity="sha256-/ijcOLwFf26xEYAjW75FizKVo5tnTYiQddPZoLUHHZ8=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"
+            integrity="sha256-OFRAJNoaD8L3Br5lglV7VyLRf0itmoBzWUoM+Sji4/8=" crossorigin="anonymous"></script>
+
+    <script src="/~cs2041/22T2/flask.cgi/static/course.js?101671653381006.0="></script>
+
+    <!-- <script async src="https://static.codepen.io/assets/embed/ei.js"></script> -->
+
+    <!-- MathJax. -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.6/latest.js?config=TeX-MML-AM_CHTML"
+            async="yes"></script>
+
+    
+    
+
+    
+        <style>
+            body {
+                background-image: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' version='1.1' height='50px' width='220px'><text x='0' y='15' fill='red' font-size='20' opacity='0.2'>  [OLD - 22T2]  </text></svg>");
+            }
+        </style>
+    
+</head>
+<body class="d-flex flex-column" style="min-height: 100vh;">
+<nav class="navbar fixed-top navbar-expand-lg navbar-light no-print" id="header-navbar">
+    <div class="container">
+        <a class="navbar-brand" href="https://cgi.cse.unsw.edu.au/~cs2041/22T2/">
+            COMP(2041|9044) - 22T2
+        </a>
+        <button class="navbar-toggler navbar-toggler-right"
+                type="button" data-toggle="collapse" data-target="#navmenu"
+                aria-controls="navmenu" aria-label="Toggle navigation" aria-expanded="false">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navmenu">
+            <ul class="navbar-nav mr-auto">
+                
+                    <li class="nav-item active">
+                        <a class="nav-link" href="https://cgi.cse.unsw.edu.au/~cs2041/22T2/outline/">
+                            Course Outline
+                        </a>
+                    </li>
+                
+                    <li class="nav-item active">
+                        <a class="nav-link" href="https://cgi.cse.unsw.edu.au/~cs2041/22T2/timetable/">
+                            Course Timetable
+                        </a>
+                    </li>
+                
+                    <li class="nav-item active">
+                        <a class="nav-link" href="https://cgi.cse.unsw.edu.au/~cs2041/22T2/forum/">
+                            Course Forum
+                        </a>
+                    </li>
+                
+                
+                
+            </ul>
+        </div>
+    </div>
+</nav>
+<main class="container" aria-label="Content" style="flex: 1; padding-top: 4rem;">
+    
+
+        <header>
+            
+                <h1 class="text-center">ass1_tigger/fri15-strings/5289988 ! test07.sh</h1>
+            
+        </header>
+
+        
+<p>
+<a class="btn btn-outline-dark" href="https://cgi.cse.unsw.edu.au/~cs2041/22T2//student/fri15-strings/5289988/submission/ass1_tigger/test07.sh/raw">Download <code></code></a>
+<p>
+
+<style>
+  
+</style>
+<Macro 'code'>
+
+    
+</main>
+<footer class="mt-3 pt-3 bg-dark text-center no-print">
+    <p class="text-muted">
+        <strong>COMP(2041|9044) 22T2: Software Construction</strong>
+        is brought to you by <br/>
+        the <a href="https://www.cse.unsw.edu.au/">School of Computer Science and Engineering</a><br/>
+        at the <a href="https://www.unsw.edu.au/">University of New South Wales</a>, Sydney.<br/>
+        For all enquiries, please email the class account at
+        <a href="mailto:@cse.unsw.edu.au">cs2041@cse.unsw.edu.au</a><br/>
+
+        <small>CRICOS Provider 00098G</small>
+    </p>
+
+    
+</footer>
+
+</body>
+</html>
